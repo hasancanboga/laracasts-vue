840cbc3bdc0443e0a6b5ca51eac096833d90739b
Hasancan Boga <hasancanboga@gmail.com>
2021-04-24 16:47:08 +0300

    finished ep 20

--- episodes/19/forms/resources/views/projects/create.blade.php
+++ episodes/19/forms/resources/views/projects/create.blade.php
@@ -17,20 +17,24 @@
 <body>
     <div id="app" class="container">
         {{-- @include ('projects.list') --}}
-        <form action="/projects" method='POST' @submit.prevent="onSubmit"> 
+        <form action="/projects" method='POST' @submit.prevent="onSubmit"
+            @keydown="form.errors.clear($event.target.name)">
 
             <div class="control">
                 <label for="name" class='label'>Project Name:</label>
-                <input type="text" id='name' name="name" class="input" v-model="name">
+                <input type="text" id='name' name="name" class="input" v-model="form.name">
+                <span class="help is-danger" v-if="form.errors.has('name')" v-text="form.errors.get('name')"></span>
             </div>
             <br>
             <div class="control">
                 <label for="description" class='label'>Project Description:</label>
-                <input type="text" id='description' name="description" class="input" v-model="description">
+                <input type="text" id='description' name="description" class="input" v-model="form.description">
+                <span class="help is-danger" v-if="form.errors.has('description')"
+                    v-text="form.errors.get('description')"></span>
             </div>
             <br>
             <div class="control">
-                <button class="button is-primary">Create</button>
+                <button class="button is-primary" :disabled="form.errors.any()">Create</button>
             </div>
 
         </form>
